e61f2230ee5edde27e481eb1cf23435b0ebf0686
bannovdaniil <baddan@mail.ru>
2023-10-09 19:34:15 +0300

    add user incoming dto

---
+++ src/main/java/ru/ylab/model/dto/UserIncomingDto.java
@@ -0,0 +1,40 @@
+package ru.ylab.model.dto;
+
+public class UserIncomingDto {
+    private String firstName;
+    private String lastName;
+    private char[] password;
+
+    public UserIncomingDto() {
+    }
+
+    public UserIncomingDto(String firstName, String lastName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password.toCharArray();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return String.valueOf(password);
+    }
+
+    public void setPassword(String password) {
+        this.password = password.toCharArray();
+    }
+}
