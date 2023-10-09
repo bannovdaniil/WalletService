7b5f77ffe358739e11e1730a7b68f50684b3bc3c
bannovdaniil <baddan@mail.ru>
2023-10-09 19:37:47 +0300

    main UI

--- src/main/java/ru/ylab/Main.java
+++ src/main/java/ru/ylab/Main.java
@@ -1,7 +1,21 @@
 package ru.ylab;
 
+import ru.ylab.util.menu.Item;
+import ru.ylab.util.menu.Menu;
+import ru.ylab.util.menu.action.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Menu menu = new Menu();
+        menu.addElement(new Item("Registration User", new UserRegistration()));
+        menu.addElement(new Item("Show User by ID", new UserFindById()));
+        menu.addElement(new Item("Show All User", new UserFindAll()));
+
+        menu.addElement(new Item("Login", new UserLogin()));
+        menu.addElement(new Item("Logout", new UserLogout()));
+
+        menu.doAction();
+        
     }
 }
