038cf3c9f09428b3a809b4f6443d279df97f3608
bannovdaniil <baddan@mail.ru>
2023-10-09 19:33:55 +0300

    add user service

---
+++ src/main/java/ru/ylab/service/UserService.java
@@ -0,0 +1,20 @@
+package ru.ylab.service;
+
+import ru.ylab.exception.NotFoundException;
+import ru.ylab.model.User;
+import ru.ylab.model.dto.UserIncomingDto;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
+public interface UserService {
+    User add(UserIncomingDto user);
+
+    User find(Long userId) throws NotFoundException;
+
+    void update(User user) throws NotFoundException, AccessDeniedException;
+
+    void delete(Long userId) throws NotFoundException;
+
+    List<User> findAll();
+}
---
+++ src/main/java/ru/ylab/service/impl/UserServiceImpl.java
@@ -0,0 +1,81 @@
+package ru.ylab.service.impl;
+
+import ru.ylab.exception.NotFoundException;
+import ru.ylab.mapper.UserDtoMapper;
+import ru.ylab.mapper.impl.UserDtoMapperImpl;
+import ru.ylab.model.User;
+import ru.ylab.model.Wallet;
+import ru.ylab.model.dto.UserIncomingDto;
+import ru.ylab.repository.UserRepository;
+import ru.ylab.repository.WalletRepository;
+import ru.ylab.repository.impl.UserRepositoryImpl;
+import ru.ylab.repository.impl.WalletRepositoryImpl;
+import ru.ylab.service.UserService;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private static UserService instance;
+    private final UserRepository userRepository = UserRepositoryImpl.getInstance();
+    private final WalletRepository walletRepository = WalletRepositoryImpl.getInstance();
+    private final UserDtoMapper userDtoMapper = UserDtoMapperImpl.getInstance();
+
+    private UserServiceImpl() {
+    }
+
+    public static synchronized UserService getInstance() {
+        if (instance == null) {
+            instance = new UserServiceImpl();
+        }
+        return instance;
+    }
+
+    @Override
+    public User add(UserIncomingDto dto) {
+        User user = userDtoMapper.map(dto);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User find(Long userId) throws NotFoundException {
+        return userRepository.findById(userId).orElseThrow(
+                () -> new NotFoundException("User not found")
+        );
+    }
+
+    @Override
+    public void update(User user) throws NotFoundException, AccessDeniedException {
+        if (user.getWallet() != null && walletRepository.exitsById(user.getWallet().getId())) {
+            Wallet wallet = walletRepository.findById(user.getWallet().getId()).orElseThrow(
+                    () -> new NotFoundException("Wallet of User not exist.")
+            );
+            if (wallet.getOwner() != null || !wallet.getOwner().getId().equals(user.getId())) {
+                throw new AccessDeniedException("This wallet does not belong to the user.");
+            }
+        }
+        if (user.getWallet() != null && user.getWallet().getId() == null) {
+            Wallet wallet = user.getWallet();
+            wallet.setOwner(user);
+            wallet = walletRepository.save(wallet);
+            user.setWallet(wallet);
+        }
+        userRepository.update(user);
+    }
+
+    @Override
+    public void delete(Long userId) throws NotFoundException {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new NotFoundException("User not found.")
+        );
+        if (user.getWallet() != null && walletRepository.exitsById(user.getWallet().getId())) {
+            walletRepository.deleteById(user.getWallet().getId());
+        }
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
