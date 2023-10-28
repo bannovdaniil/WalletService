3d8be049429188d7060eaabf66b94353cf33ce50
bannovdaniil <baddan@mail.ru>
2023-10-28 21:40:01 +0300

    fix review

--- src/main/java/ru/ylab/Constants.java
+++ src/main/java/ru/ylab/Constants.java
@@ -1,12 +1,15 @@
 package ru.ylab;
 
-import java.time.format.DateTimeFormatter;
-
+/**
+ * Константы:
+ * REGEXP_FORMAT_MONEY - денежный формат
+ * SESSION_COOKIE - наименование куки для сессии
+ * DATE_TIME_STRING - отображение даты для json
+ */
 public class Constants {
     public static final String REGEXP_FORMAT_MONEY = "^\\d*([\\.,]\\d{1,2})?$";
     public static final String SESSION_COOKIE = "session";
     public static final String DATE_TIME_STRING = "yyyy-MM-dd HH:mm:ss";
-    public static final DateTimeFormatter DATE_TIME_SPACE = DateTimeFormatter.ofPattern(DATE_TIME_STRING);
 
     private Constants() {
     }
