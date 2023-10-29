7679f307ec1f4d022d482a0b82c55c744f7b45d8
bannovdaniil <baddan@mail.ru>
2023-10-30 01:07:13 +0300

    add test Validation Test

---
+++ src/test/java/ru/ylab/validator/impl/UserLoginDtoValidatorImplTest.java
@@ -0,0 +1,23 @@
+package ru.ylab.validator.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.ylab.model.dto.UserLoginDto;
+
+class UserLoginDtoValidatorImplTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 'password', true",
+            "0, 'password', false",
+            "0, '', false"
+    })
+    void isValid(Long id, String password, Boolean exceptedValue) {
+        UserLoginDtoValidatorImpl userLoginDtoValidator = new UserLoginDtoValidatorImpl();
+
+        UserLoginDto dto = new UserLoginDto(id, password);
+
+        Assertions.assertEquals(exceptedValue, userLoginDtoValidator.isValid(dto, null));
+    }
+}
---
+++ src/test/java/ru/ylab/validator/impl/WalletIncomingDtoValidatorImplTest.java
@@ -0,0 +1,38 @@
+package ru.ylab.validator.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.ylab.model.dto.BalanceType;
+import ru.ylab.model.dto.WalletIncomingDto;
+
+class WalletIncomingDtoValidatorImplTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "GET, '10', true",
+            "GET, '10.01', true",
+            "GET, '10,01', true",
+            "PUT, '10', true",
+            "GET, '10,001', false",
+            "GET, '', false",
+            "GET, '-1', false",
+            "GE, '10', false",
+            "0, '100', false"
+    })
+    void isValid(String type, String sum, Boolean exceptedValue) {
+        WalletIncomingDtoValidatorImpl dtoValidator = new WalletIncomingDtoValidatorImpl();
+        WalletIncomingDto dto = null;
+        try {
+            dto = new WalletIncomingDto(
+                    BalanceType.valueOf(type),
+                    sum
+            );
+        } catch (Throwable ex) {
+
+        }
+
+        Assertions.assertEquals(exceptedValue, dtoValidator.isValid(dto, null));
+    }
+
+}
