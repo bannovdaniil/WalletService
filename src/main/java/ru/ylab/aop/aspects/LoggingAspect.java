8b67f0920ada0eafd143217f5eb6d0d812497e8d
bannovdaniil <baddan@mail.ru>
2023-10-30 01:50:24 +0300

    refactoring Aspect

--- src/main/java/ru/ylab/aop/aspects/LoggingAspect.java
+++ src/main/java/ru/ylab/aop/aspects/LoggingAspect.java
@@ -5,16 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import ru.ylab.model.Action;
 import ru.ylab.service.ActionService;
 import ru.ylab.service.SessionService;
 
-import java.lang.reflect.Modifier;
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.StringJoiner;
 import java.util.UUID;
 
 @Component
@@ -29,50 +28,28 @@ public class LoggingAspect {
             "|| ru.ylab.aop.aspects.CommonPointcuts.isPutMapping(cookie) " +
             "|| ru.ylab.aop.aspects.CommonPointcuts.isPostMapping(cookie)")
     public Object addLogging(ProceedingJoinPoint joinPoint, String cookie) throws Throwable {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        StringJoiner jsonJoiner = new StringJoiner(", ");
 
-        System.out.println("full method description: " + signature.getMethod());
-        System.out.println("method name: " + signature.getMethod().getName());
-        System.out.println("declaring type: " + signature.getDeclaringType());
-
-        // Method args
-        System.out.println("Method args names:");
-        Arrays.stream(signature.getParameterNames())
-                .forEach(s -> System.out.println("arg name: " + s));
-
-        System.out.println("Method args types:");
-        Arrays.stream(signature.getParameterTypes())
-                .forEach(s -> System.out.println("arg type: " + s));
-
-        System.out.println("Method args values:");
-        Arrays.stream(joinPoint.getArgs())
-                .forEach(o -> System.out.println("arg value: " + o.toString()));
-
-        // Additional Information
-        System.out.println("returning type: " + signature.getReturnType());
-        System.out.println("method modifier: " + Modifier.toString(signature.getModifiers()));
-        Arrays.stream(signature.getExceptionTypes())
-                .forEach(aClass -> System.out.println("exception type: " + aClass));
-
-
-        log.info("Rest Controller");
-        Action action;
-        Object result = null;
+        for (Object s : joinPoint.getArgs()) {
+            if (!Objects.equals(s, cookie)) {
+                jsonJoiner.add(s.toString());
+            }
+        }
 
         Long userId = 0L;
         String session = "not login";
-        String path = "";
-        String json = "";
 
         long startTime = System.currentTimeMillis();
         long endTime = 0L;
         try {
-            result = joinPoint.proceed();
+            Object result = joinPoint.proceed();
             endTime = System.currentTimeMillis() - startTime;
             return result;
         } catch (Throwable ex) {
+            jsonJoiner.add("Exception: " + ex.getMessage());
             throw ex;
         } finally {
+            String json = jsonJoiner.toString();
             if (cookie != null && !cookie.isBlank()) {
                 Optional<UUID> sessionId = sessionService.getUuidFromCookie(cookie);
                 if (sessionId.isPresent() && sessionService.isActive(sessionId.get())) {
@@ -81,18 +58,17 @@ public class LoggingAspect {
                 }
             }
 
-            action = new Action(
+            Action action = new Action(
                     LocalDateTime.now(),
                     joinPoint.getSignature().toShortString(),
                     userId,
-                    String.format("time: %d ms, session: [%s], path: [%s], json: %s",
+                    String.format("time: %d ms, session: [%s], json: %s",
                             endTime,
                             session,
-                            path == null ? "" : path,
                             json
                     )
             );
-            actionService.add(action);
+            action = actionService.add(action);
             log.info("{}", action);
         }
     }
--- src/main/java/ru/ylab/model/dto/UserLoginDto.java
+++ src/main/java/ru/ylab/model/dto/UserLoginDto.java
@@ -4,7 +4,10 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import ru.ylab.validator.UserLoginDtoValidation;
+
+import java.util.StringJoiner;
 
 /**
  * Для передачи данных при логине.
@@ -12,9 +15,18 @@ import ru.ylab.validator.UserLoginDtoValidation;
 @UserLoginDtoValidation
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginDto {
     @Positive
     private Long userId;
     @Size(min = 1)
     private String password;
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", UserLoginDto.class.getSimpleName() + "[", "]")
+                .add("userId=" + userId)
+                .add("password='*****'")
+                .toString();
+    }
 }
