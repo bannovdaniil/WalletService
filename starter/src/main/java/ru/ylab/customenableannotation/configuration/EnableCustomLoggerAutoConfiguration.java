d07054ccf2bef180403a0f3cb509bbf5f0f6b33f
bannovdaniil <baddan@mail.ru>
2023-11-08 10:07:55 +0300

    fix2 @EnableCustomLogger

--- starter/src/main/java/ru/ylab/customenableannotation/configuration/EnableCustomLoggerAutoConfiguration.java
+++ starter/src/main/java/ru/ylab/customenableannotation/configuration/EnableCustomLoggerAutoConfiguration.java
@@ -4,12 +4,10 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import ru.ylab.customenableannotation.aop.aspects.CustomLogger;
 import ru.ylab.customenableannotation.aop.aspects.EnableCommonPointcuts;
 
 @Slf4j
-@Configuration
 public class EnableCustomLoggerAutoConfiguration {
     @PostConstruct
     void init() {
